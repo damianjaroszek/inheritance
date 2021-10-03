b73b01944a0595ca7e1bd7c632ae8d0de7ff8521
unknown <deejay.8lm@gmail.com>
2021-10-03 20:26:18 +0200

    pozyczanie konstruktora

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,37 @@
-function Person(firstName, lastName) {
-    this.fName = firstName;
-    this.lName = lastName;
+/*
 
-    this.sayHello = function() {
-        return this.fName + " " + this.lName;
+Shape
+    Rectangle
+        Square
+    Triangle
+
+ */
+
+    function Shape(sideLengths){
+        this._name = "";
+        this._sideLengths = sideLengths;
+    }
+
+    Shape.prototype.getPerimeter = function(){ //oblicz obwód
+        return this._sideLengths.reduce(function(prevVal, val) { //sumowanie wartości z tablicy
+            return prevVal + val;
+        });
     };
-}
 
-var person1 = new Person("Jan", "Kowalski");
-var person2 = new Person("Tomasz", "Nowak");
+    Shape.prototype.getArea = function(){ //oblicz pole
+        return this._sideLengths[0] * this._sideLengths[1]; //a*b
+    };
+
+    function Rectangle(sideLengths) {
+        Shape.call(this, [sideLengths[0], sideLengths[1], sideLengths[0], sideLengths[1]]); //pożyczenie konstruktora
+        this._name = "Prostokąt";
+    }
+
+    function Square(sideLength){
+        Rectangle.call(this, [sideLength, sideLength]);
+
+        this._name = "Kwadrat"
+    }
+
+
+    var shape1 = new Square(20);
