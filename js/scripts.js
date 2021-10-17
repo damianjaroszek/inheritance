a83c5e88f01a82231e116c4f0341b4aad104037c
unknown <deejay.8lm@gmail.com>
2021-10-17 18:45:02 +0200

    poprawne dziedziczenie pól i metod z klasy bazowej

--- js/scripts.js
+++ js/scripts.js
@@ -6,32 +6,67 @@ Shape
     Triangle
 
  */
-
+    //KLASA BAZOWA SHAPE ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     function Shape(sideLengths){
-        this._name = "";
-        this._sideLengths = sideLengths;
+        this._name;                 // nazwa figury - pole będzie nadpisywane
+        this._sideLengths=sideLengths; // tablica będzie przechowywała długości max 4 boków dlatego [sideLengths[0], sideLengths[1], sideLengths[0], sideLengths[1]] => a+b+a+b (prostokąt)       
     }
 
-    Shape.prototype.getPerimeter = function(){ //oblicz obwód
-        return this._sideLengths.reduce(function(prevVal, val) { //sumowanie wartości z tablicy
-            return prevVal + val;
+    Shape.prototype.getPerimeter = function(){ // metoda klasy Shape licząca pole (sumująca wszystkie boki) --> sumuje wszystkie wartości tablicy - dostępna dla wszystkich obiektów
+        return this._sideLengths.reduce(function(prevVal, val){
+            return prevVal+val;
         });
     };
 
-    Shape.prototype.getArea = function(){ //oblicz pole
-        return this._sideLengths[0] * this._sideLengths[1]; //a*b
+    Shape.prototype.getArea = function(){
+        return this._sideLengths[0]*this._sideLengths[1];
     };
 
-    function Rectangle(sideLengths) {
-        Shape.call(this, [sideLengths[0], sideLengths[1], sideLengths[0], sideLengths[1]]); //pożyczenie konstruktora
-        this._name = "Prostokąt";
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    // PROSTOKĄT /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    function Rectangle(sideLengths){
+        Shape.call(this, [sideLengths[0], sideLengths[1], sideLengths[0], sideLengths[1]]);
+        this.name="Prostokąt";
     }
 
-    function Square(sideLength){
-        Rectangle.call(this, [sideLength, sideLength]);
+    Rectangle.prototype=Object.create(Shape.prototype);
+    Rectangle.prototype.constructor=Rectangle;
 
-        this._name = "Kwadrat"
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    // KWADRAT /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    function Square(sideLengths){//                                                                                                                 0,1
+        Shape.call(this, [sideLengths, sideLengths]); //nie wysyłam wybranych elementów tablicy tylko dwie takie same tablice z tą samą wartością [20,20] więc 
+        this.name="Kwadrat";                          // w getArea też będzie działało this._sideLengths[0]*this._sideLengths[1] --> 20*20
     }
 
+    Square.prototype=Object.create(Shape.prototype);
+    Square.prototype.constructor=Square;
 
-    var shape1 = new Square(20);
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    //TRÓJKĄT RÓWNOBOCZNY
+    function Triangle(sideLengths){
+        Shape.call(this, [sideLengths, sideLengths, sideLengths]);
+        this.name="Trójkat";
+    }
+
+    Triangle.prototype=Object.create(Shape.prototype);
+    Triangle.prototype.constructor=Triangle;
+
+    function Triangle(sideLengths){
+        var a = _sideLengths[0];
+        return +((a*a*Math.sqrt(3))/4).toFixed(2);
+
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    var prostokat = new Rectangle([200, 10]);
+    var kwadrat = new Square(20);
+    var trojkat = new Triangle(10);
